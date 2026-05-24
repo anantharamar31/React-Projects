45035a3e220aabe141794fd8c26c0baffc32ef46
Anantharamar A <anantharamar31.a@gmail.com>
2026-05-24 23:33:20 +0530

    Update base path in Vite configuration

--- birthday-surprise/vite.config.js
+++ birthday-surprise/vite.config.js
@@ -3,6 +3,5 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [react()],
-  // IMPORTANT: Change '/birthday-surprise/' to '/YOUR-REPO-NAME/'
-  base: '/birthday-surprise/',
+  base: '/React-Projects/',
 })
